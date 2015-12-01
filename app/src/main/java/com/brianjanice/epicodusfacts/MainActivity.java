b2934fd2c3ea481ca8aec7c37c571dec441b0478
Brian Lau & Janice Laset-Parkerson <student@epicodus.com>
2015-11-30 16:22:03 -0800

    display random epicodus facts

--- app/src/main/java/com/brianjanice/epicodusfacts/MainActivity.java
+++ app/src/main/java/com/brianjanice/epicodusfacts/MainActivity.java
@@ -1,21 +1,29 @@
 package com.brianjanice.epicodusfacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         final Button showFactButton = (Button)findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        String fact = mFactBook.getFact();
+        // update label with dynamic fact
+        factLabel.setText(fact);
 
         View.OnClickListener listener = new View.OnClickListener() {
 
@@ -35,10 +48,15 @@ public class MainActivity extends AppCompatActivity {
                 // update label with dynamic fact
                 factLabel.setText(fact);
 
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
 
+        //Toast.makeText(this, "Yay! Our activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "We're logging from the onCreate method");
 
     }
 
